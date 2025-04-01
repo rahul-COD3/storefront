9fba7c6de1917937134ca07020d7f49d38a4bd23
rahul-cod3 <rahul@promactinfo.com>
2025-04-01 15:30:08 +0530

    Add view and URL configuration for playground app

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# request -> response
+# request handler
+# action
+
+def say_hello(request):
+    return render(request, 'hello.html')
+    
