dc6ae3e70dc02ab6dc3b812abb993160f3600d3f
rahul-COD3 <r1303yadav@gmail.com>
2025-04-03 11:21:57 +0530

    Update database configuration in settings.py to use PostgreSQL instead of SQLite

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
