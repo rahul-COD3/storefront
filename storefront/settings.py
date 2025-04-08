80e248f7351b7ca06053295c4d53715c4e67a89e
Rahul Kumar <rahul@promactinfo.com>
2025-04-09 00:01:17 +0530

    Update settings.py: add 'rest_framework' to INSTALLED_APPS and change database name from 'storefront' to 'storefront2'

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "playground",
     "debug_toolbar",
     "store",
@@ -87,7 +88,7 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "storefront",
+        "NAME": "storefront2",
         "USER": "postgres",
         "PASSWORD": "root",
         "HOST": "localhost",
