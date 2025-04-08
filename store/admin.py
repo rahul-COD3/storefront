cdd6cd0e834137bfbdd2a3ab00e58859197a2af8
Rahul Kumar <rahul@promactinfo.com>
2025-04-08 11:48:20 +0530

    Add inventory filter and enhance Customer and Collection admin views with order links and improved display options

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from . import models
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request, model_admin):
+        return [('<10', 'Low'), ('>10', 'Ok')]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        if self.value() == '>10':
+            return queryset.filter(inventory__gt=10)
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "unit_price", "inventory_status", "collection"]
     list_per_page = 10
+    list_filter = ['collection', 'last_update', InventoryFilter]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, obj):
@@ -15,9 +32,18 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "membership", "orders"]
     list_editable = ["membership"]
     list_per_page = 10
+    ordering = ["first_name", "last_name"]
+    search_fields = ["first_name__istartswith"]
+
+    def orders(self, obj):
+        url = f"{reverse('admin:store_order_changelist')}?{urlencode({'customer__id': obj.id})}"
+        return format_html("<a href='{}'>{}</a>", url, obj.orders)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders=Count("order"))
 
 
 @admin.register(models.Collection)
@@ -27,7 +53,8 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering="products__count")
     def product_count(self, obj):
-        return obj.products__count
+        url = f"{reverse('admin:store_product_changelist')}?{urlencode({'collection__id': obj.id})}"
+        return format_html("<a href='{}'>{}</a>", url, obj.products__count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products__count=Count("product"))
