9c4441c15fab89518d006875362f9ef9e909a979
Rahul Kumar <rahul@promactinfo.com>
2025-04-09 11:55:54 +0530

    Update OrderItem model to include related_name for product and enhance product_detail view to support PUT and DELETE methods with validation for order item associations.

--- store/views.py
+++ store/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
@@ -19,10 +18,22 @@ def product_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    return Response(ProductSerializer(product).data)
+    if request.method == 'GET':
+        return Response(ProductSerializer(product).data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({"error": "Product cannot be deleted because it is associated with an order item."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view()
 def collection_details(request, pk):
