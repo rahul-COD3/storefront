052811d6709c31a9c401fba07ded29e434d83ac3
Rahul Kumar <rahul@promactinfo.com>
2025-04-10 12:06:12 +0530

    Refactor views and URLs to use class-based views: Update ProductDetail and CollectionDetail to use RetrieveUpdateDestroyAPIView for improved structure and consistency.

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:id>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
     path('collections/', views.CollectionList.as_view(), name='collection-list'),
-    path('collections/<int:pk>/', views.collection_details, name='collection-detail'),
+    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
 ]
 
