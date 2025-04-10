2187f9314e3654044ca773854e50e385f7b62ce5
rahul-COD3 <r1303yadav@gmail.com>
2025-04-10 13:03:53 +0530

    Add Review model and update views and URLs to use viewsets: Implement Review model with fields for product, name, description, and date. Refactor product and collection views to use ModelViewSet for improved structure and consistency.

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('collections/', views.CollectionList.as_view(), name='collection-list'),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
+urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -2,40 +2,29 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Collection, Product
+from rest_framework.viewsets import ModelViewSet
+from .models import Collection, OrderItem, Product
 from .serializers import CollectionSerializer, ProductSerializer
 from rest_framework import status
 
-
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product__id=kwargs['pk']).count() > 0:
             return Response({"error": "Product cannot be deleted because it is associated with an order item."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+    
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.all()
-    serializer_class = CollectionSerializer
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection__id=kwargs['pk']).count() > 0:
             return Response({"error": "Collection cannot be deleted because it is associated with a product."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
