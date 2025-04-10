caa34760565b613b33c5916c45af00275dbfd412
rahul-COD3 <r1303yadav@gmail.com>
2025-04-10 14:53:50 +0530

    Add Review model and implement ReviewViewSet: Create ReviewSerializer and update URLs to support nested routing for product reviews.

--- store/views.py
+++ store/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-from .models import Collection, OrderItem, Product
-from .serializers import CollectionSerializer, ProductSerializer
+from .models import Collection, OrderItem, Product, Review
+from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from rest_framework import status
 
 class ProductViewSet(ModelViewSet):
@@ -28,3 +26,10 @@ class CollectionViewSet(ModelViewSet):
         if Product.objects.filter(collection__id=kwargs['pk']).count() > 0:
             return Response({"error": "Collection cannot be deleted because it is associated with a product."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
