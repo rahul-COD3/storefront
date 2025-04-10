5460b97e031a7ef7bd98845f8d962d07e637be88
Rahul Kumar <rahul@promactinfo.com>
2025-04-10 15:37:54 +0530

    Add Django Filter integration for ProductViewSet: Implement ProductFilter to enable filtering by collection and unit price.

--- store/views.py
+++ store/views.py
@@ -1,19 +1,18 @@
 from django.db.models import Count
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
+
+from .filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from rest_framework import status
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id:
-            queryset = queryset.filter(collection__id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {"request": self.request}
