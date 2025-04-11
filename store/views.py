1e55e6e9dd173ecc483577103732afb102850d47
Rahul Kumar <rahul@promactinfo.com>
2025-04-11 12:06:07 +0530

    Add Cart functionality: Implement CartViewSet and CartSerializer, and register Cart routes in the API

--- store/views.py
+++ store/views.py
@@ -3,9 +3,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.viewsets import ModelViewSet
-from .models import Collection, OrderItem, Product, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import Cart, Collection, OrderItem, Product, Review
+from .serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 from .pagination import DefaultPagination
 from .filters import ProductFilter
 
@@ -44,3 +45,7 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+
+class CartViewSet(GenericViewSet, CreateModelMixin):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
