ed4b6bd415be3ed5160bdacac3efa9793190227a
rahul-COD3 <r1303yadav@gmail.com>
2025-04-13 15:11:09 +0530

    Add CreateOrderSerializer and update OrderViewSet to handle order creation

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from django.db import transaction
 from rest_framework import serializers
 from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 
@@ -110,7 +111,31 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Order
         fields = ['id', 'customer', 'placed_at', 'payment_status', 'items']
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def save(self, **kwargs):
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            customer, created = Customer.objects.get_or_create(user_id=self.context['user_id'])
+
+            order = Order.objects.create(
+                customer=customer
+            )
+            
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order = order,
+                    product = item.product,
+                    quantity = item.quantity,
+                    unit_price = item.product.unit_price
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=cart_id).delete()
