9d0a8f766fee0df0731c3a2189b5fe6c9ad8f9ce
rahul-cod3 <rahul@promactinfo.com>
2025-04-19 15:09:30 +0530

    Refactor collection tests to use create_collection fixture; simplify test case implementations

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,48 +1,55 @@
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 
 
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post("/store/collections/", data=collection)
+
+    return do_create_collection
+
+
 @pytest.mark.django_db
 class TestCollections:
-    def test_if_user_is_anonymous_returns_401(self, api_client):
+    def test_if_user_is_anonymous_returns_401(self, api_client, create_collection):
         # Arrange
 
         # Act
-        response = api_client.post("/store/collections/", data={"title": "Test"})
+        response = create_collection({"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self, api_client):
+    def test_if_user_is_not_admin_returns_403(self, api_client, create_collection):
         # Arrange
 
         api_client.force_authenticate(user={})
 
         # Act
-        response = api_client.post("/store/collections/", data={"title": "Test"})
+        response = create_collection({"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_check_if_data_is_invalid_returns_400(self, api_client):
+    def test_check_if_data_is_invalid_returns_400(self, api_client, create_collection):
         # Arrange
         api_client.force_authenticate(user=(User(is_staff=True)))
 
         # Act
-        response = api_client.post("/store/collections/", data={"title": ""})
+        response = create_collection({"title": ""})
 
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data["title"] is not None
 
-    def test_check_if_data_is_valid_returns_201(self, api_client):
+    def test_check_if_data_is_valid_returns_201(self, api_client, create_collection):
         # Arrange
         api_client.force_authenticate(user=(User(is_staff=True)))
 
         # Act
-        response = api_client.post("/store/collections/", data={"title": "Test"})
+        response = create_collection({"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
