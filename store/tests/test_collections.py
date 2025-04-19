d3a3aea45becd729b926f40c9f47381206d5c648
rahul-COD3 <r1303yadav@gmail.com>
2025-04-19 15:00:09 +0530

    Refactor tests to use shared api_client fixture; simplify test cases for user authentication

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -6,46 +6,43 @@ import pytest
 
 @pytest.mark.django_db
 class TestCollections:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, api_client):
         # Arrange
 
         # Act
-        client = APIClient()
-        response = client.post("/store/collections/", data={"title": "Test"})
+        response = api_client.post("/store/collections/", data={"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self):
+    def test_if_user_is_not_admin_returns_403(self, api_client):
         # Arrange
-        client = APIClient()
-        client.force_authenticate(user={})
+
+        api_client.force_authenticate(user={})
 
         # Act
-        response = client.post("/store/collections/", data={"title": "Test"})
+        response = api_client.post("/store/collections/", data={"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_check_if_data_is_invalid_returns_400(self):
+    def test_check_if_data_is_invalid_returns_400(self, api_client):
         # Arrange
-        client = APIClient()
-        client.force_authenticate(user=(User(is_staff=True)))
+        api_client.force_authenticate(user=(User(is_staff=True)))
 
         # Act
-        response = client.post("/store/collections/", data={"title": ""})
+        response = api_client.post("/store/collections/", data={"title": ""})
 
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data["title"] is not None
 
-    def test_check_if_data_is_valid_returns_201(self):
+    def test_check_if_data_is_valid_returns_201(self, api_client):
         # Arrange
-        client = APIClient()
-        client.force_authenticate(user=(User(is_staff=True)))
+        api_client.force_authenticate(user=(User(is_staff=True)))
 
         # Act
-        response = client.post("/store/collections/", data={"title": "Test"})
+        response = api_client.post("/store/collections/", data={"title": "Test"})
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
