1e10942f2a72c09d7b5cfeed632d2a7fe973f75c
rahul-COD3 <r1303yadav@gmail.com>
2025-04-19 10:40:02 +0530

    Implement email sending functionality in say_hello view; add SMTP settings and email template

--- storefront/settings.py
+++ storefront/settings.py
@@ -172,3 +172,12 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=8),
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "from@rahul.com"
+
+ADMINS = [("Rahul", "admin@rahul.com")]
