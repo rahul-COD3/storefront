14d0cd4cda0da0f1243c8ffbb26a21cb6c9c3b20
rahul-COD3 <r1303yadav@gmail.com>
2025-04-20 12:38:30 +0530

    Refactor hello view to use APIView; implement caching and external request handling

--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    path('hello/', views.say_hello, name='hello')
-]
+urlpatterns = [path("hello/", views.HelloView.as_view(), name="hello")]
